fe5fa763c4870d28828d913dcc4cbc93c2e1349c
hyeyoom <neoul_chw@icloud.com>
2018-01-15 14:37:03 +0900

    initial commit

---
+++ src/main/java/xyz/neoul/papyrus/PapyrusApplication.java
@@ -0,0 +1,12 @@
+package xyz.neoul.papyrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapyrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapyrusApplication.class, args);
+	}
+}
