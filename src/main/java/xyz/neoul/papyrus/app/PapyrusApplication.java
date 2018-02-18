8c12d4d601c0e1bcaf651838ea75f146497bdd87
hyeyoom <neoul_chw@icloud.com>
2018-02-18 22:23:23 +0900

    frontend integration
    frontend integration

--- src/main/java/xyz/neoul/papyrus/app/PapyrusApplication.java
+++ src/main/java/xyz/neoul/papyrus/app/PapyrusApplication.java
@@ -1,8 +1,10 @@
-package xyz.neoul.papyrus;
+package xyz.neoul.papyrus.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("xyz.neoul.papyrus.*")
 @SpringBootApplication
 public class PapyrusApplication {
 
