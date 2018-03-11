8156ec16807629bcdc643eb1e76a3631e1f59196
kth <kthiswell@gmail.com>
2018-03-11 21:00:46 +0900

    18-03-11/kth/change vue configuration

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to front of Papyrus'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  background-color: aquamarine;
+
+}
+
+
+</style>
